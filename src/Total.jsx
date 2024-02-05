bf15dd1ea46734284e60cb8012826c5af5755efc
Fleki <marc.fleck@mataro.epiaedu.cat>
2024-02-05 15:20:59 +0100

    fix repo

--- src/Total.jsx
+++ src/Total.jsx
@@ -1,6 +1,6 @@
 export const Total = ({ exercises1, exercises2, exercises3 }) => {
 
-    const sum = exercises1 + exercises2 + exercises3;
+    const total = exercises1 + exercises2 + exercises3;
 
     return (
         <p>Number of exercises {sum}</p>
